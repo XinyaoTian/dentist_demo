137e1734a208e4e7f0cbf6d7c13e9a2b4a7533a6
XinyaoTian <31195026+XinyaoTian@users.noreply.github.com>
2018-09-03 18:10:31 +0800

    准备部署 运行dentist_demo/dentist_demo.py即可开始网站服务

--- app/__init__.py
+++ app/__init__.py
@@ -2,8 +2,7 @@ from flask import Flask
 from config import Config
 
 application = Flask(__name__)
+
 application.config.from_object(Config)
-if __name__ == '__main__':
-    application.run(host='0.0.0.0',port=5000,debug=False)
 
 from app import routes
